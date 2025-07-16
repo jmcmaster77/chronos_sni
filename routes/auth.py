7eca9ccf360052291b7964f9f32b9c97bce2c859
Jorge Martin <jmcmaster77@gmail.com>
2025-07-16 05:15:04 -0400

    se mejora el logger agregando data cuando se hace una peticion get la funcion pienso alojarla en otro archivo pero por lo momentos ya se que funciona

--- routes/auth.py
+++ routes/auth.py
@@ -1,8 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from utils.log import logger
+from user_agents import parse
+
 authentication = Blueprint("authentication", __name__)
+
+
+def datos_log(url, ip, user_agent):
+    user_agent = parse(user_agent)
+    navegador = user_agent.browser.family
+    sistema = user_agent.os.family
+    dispositivo = user_agent.device.family
+
+    datos = (
+        f"GET {url} - "
+        f"IP: {ip} - "
+        f"Navegador: {navegador} SO: {sistema}, Dispositivo: {dispositivo}"
+    )
+    return datos
+
 
 @authentication.route("/")
 def index():
-    logger.info("Peticion get")
+    datos_request = datos_log(
+        request.url, request.remote_addr, request.headers.get('User-Agent'))
+    print(datos_request)
+    logger.info(datos_request)
     return render_template("auth/login.html")
